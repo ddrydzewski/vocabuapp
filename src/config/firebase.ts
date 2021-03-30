c3ae501fc0c0868d2d95de11899ee02125427ed0
Dawid Rydzewski <38885648+ddrydzewski@users.noreply.github.com>
2021-03-30 10:53:12 +0200

    Delete firebase.ts

--- src/config/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAOzVCd-g3lCUdB6TpcGZXVInZU6T6Cook",
-  authDomain: "vocabuapp.firebaseapp.com",
-  projectId: "vocabuapp",
-  storageBucket: "vocabuapp.appspot.com",
-  messagingSenderId: "896534916998",
-  appId: "1:896534916998:web:fe31401b5824b594b7c607",
-  measurementId: "G-RGTQXR1FBD",
-};
