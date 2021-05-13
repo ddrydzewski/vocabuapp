ce2b29ed713e26a608cc046e96ad1aa9f534ce61
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-05-13 13:14:38 +0200

    fix colorss

--- src/design/colorStyles/colorStyles.ts
+++ src/design/colorStyles/colorStyles.ts
@@ -1,7 +1,7 @@
 export const colors = {
-    primeColor: "eee2dc",
-    secondaryColor: "bab2b5",
-    thirdColor: "edc7b7",
-    darkColor: "123c69",
-    bonusColor: "ac3b61"
+    primeColor: "#eee2dc",
+    secondaryColor: "#bab2b5",
+    thirdColor: "#edc7b7",
+    darkColor: "#123c69",
+    bonusColor: "#ac3b61"
 }
