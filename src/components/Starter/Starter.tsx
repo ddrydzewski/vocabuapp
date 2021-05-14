4a8426ac65d035ed80e6730415536adb466c0a2e
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-05-14 17:27:34 +0200

    Starter fix

--- src/components/Starter/Starter.tsx
+++ src/components/Starter/Starter.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Button, Jumbotron } from "react-bootstrap";
+import { firebaseApp } from "../../database/core";
 import { StartText } from "./style";
 
 export const Starter = () => {
   return (
     <StartText>
-      <h1>Learning with Vocabu</h1>
-      <h2>easy and simple</h2>
-      <Button variant="secondary" size="lg">
-        <NavLink to="/signin">Get Started</NavLink>
-      </Button>
+      <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          {!(firebaseApp.auth().currentUser)  &&
+          (
+            <Button variant="primary" href="#/signin">
+              Sign in
+            </Button>
+          )}
+        </p>
+      </Jumbotron>
     </StartText>
   );
 };
