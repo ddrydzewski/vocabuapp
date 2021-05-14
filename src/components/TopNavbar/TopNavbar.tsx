4d99e424b786929eba4598d404b03f81a8bf63bb
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-05-14 11:23:12 +0200

    Little improvment in TopNavbar

--- src/components/TopNavbar/TopNavbar.tsx
+++ src/components/TopNavbar/TopNavbar.tsx
@@ -11,23 +11,25 @@ export const TopNavbar: React.FC<IProps> = ({ userName }) => {
 
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#/">
+      <Navbar.Brand
+        href="#/"
+        style={{ fontWeight: "bold", fontSize: "22px", marginLeft: "10px" }}
+      >
         Vocabu
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#/">Home</Nav.Link>
+        <Nav className="mr-auto" style={{ marginLeft: "15px" }}>
           <Nav.Link href="#words">Words</Nav.Link>
           <NavDropdown title="Vocabu Games" id="basic-nav-dropdown">
             <NavDropdown.Item href="#test">Learn</NavDropdown.Item>
             <NavDropdown.Item href="#random">Random</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item>Separated link</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Form inline>
-          <Badge style={{marginRight: "15px"}} variant="secondary">{userName}</Badge>
+          <Badge style={{ marginRight: "15px" }} variant="secondary">
+            {userName}
+          </Badge>
           <Button variant="outline-success" href="#/" onClick={handleLogout}>
             Log out
           </Button>
@@ -37,22 +39,5 @@ export const TopNavbar: React.FC<IProps> = ({ userName }) => {
   );
 };
 
-// {/* <HeaderContainer>
-//         <LinkStyled style={{ marginLeft: "30px", padding: "15px" }}>
-//           <NavLink exact to="/">
-//             Vocabu
-//           </NavLink>
-//         </LinkStyled>
-//       {/* <AuthContainer >
-//         <LinkStyled>
-//           <NavLink exact to="/">
-//             Log in
-//           </NavLink>
-//         </LinkStyled>
-//         <LinkStyled>
-//           <NavLink exact to="/">
-//             Sign up
-//           </NavLink>
-//         </LinkStyled>
-//       </AuthContainer> */}
-//     </HeaderContainer> */}
+// {/* <NavDropdown.Divider />
+// <NavDropdown.Item>Separated link</NavDropdown.Item> */}
