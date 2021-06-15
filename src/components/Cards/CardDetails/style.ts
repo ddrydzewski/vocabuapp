a0e13cd458320cbf5a895964b7f5ea4247515c4d
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-06-15 11:38:32 +0200

    styling cardContainer

--- src/components/Cards/CardDetails/style.ts
+++ src/components/Cards/CardDetails/style.ts
@@ -4,19 +4,12 @@ import { colors } from "../../../design/colorStyles/colorStyles";
 export const CardContainer = styled.div`
   background-color: ${colors.primeColor};
   border-radius: 5px;
-  text-align: center;
-  justify-content: center;
-  height: 125px;
   margin: auto;
-  padding-top: 3%;
   margin-top: 12px;
-  margin-left: 18px;
   margin-bottom: 1rem;
-  margin-right: 1rem;
-  display: inline-block;
-  background-image: url("../../design/backgrounds/gray-lines.png");
-  width: 250px;
-  cursor: pointer;
+  overflow-wrap: break-word;
+  width: 200px;
+  position: relative;
   box-shadow: 0 6px 8px rgba(0, 0, 0, 0.12), 0 3px 4px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 
@@ -27,47 +20,26 @@ export const CardContainer = styled.div`
 
 export const WordContainer = styled.div`
   font-size: 25px;
-  text-align: center;
-  justify-content: center;
   font-weight: bold;
   color: #05386b;
-  display: block;
 `;
 
 export const IconContainer = styled.div`
-  top: 0;
-  right: 0;
-  bottom: 0;
-  flex-shrink: 0;
-  display: block;
+  display: inline-block;
 `;
 
+export const DropdownContainer = styled.div`
+  position: absolute;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  bottom: 0;
+  right: 0;
+`;
 
-
-// &[wobble='1'] {
-//   -webkit-animation: flip-horizontal-bottom 0.4s
-//     cubic-bezier(0.455, 0.03, 0.515, 0.955) both;
-//   animation: flip-horizontal-bottom 0.4s
-//     cubic-bezier(0.455, 0.03, 0.515, 0.955) both;
-// }
-
-// @-webkit-keyframes flip-horizontal-bottom {
-//   0% {
-//     -webkit-transform: rotateX(0);
-//     transform: rotateX(0);
-//   }
-//   100% {
-//     -webkit-transform: rotateX(-180deg);
-//     transform: rotateX(-180deg);
-//   }
-// }
-// @keyframes flip-horizontal-bottom {
-//   0% {
-//     -webkit-transform: rotateX(0);
-//     transform: rotateX(0);
-//   }
-//   100% {
-//     -webkit-transform: rotateX(-180deg);
-//     transform: rotateX(-180deg);
-//   }
-// }
+export const NoteContainer = styled.div`
+  position: absolute;
+  margin-left: 5px;
+  font-size: 20px;
+  bottom: 0;
+  left: 0;
+`;
