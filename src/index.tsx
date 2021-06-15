579c4b5274103850741f7c5a5a3e44dfa1e9f276
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-06-15 11:37:15 +0200

    Fix warning about reactModal

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 import App from './App';
 import { AppStateProvider } from './context/state';
 import './index.css';
@@ -12,5 +13,11 @@ ReactDOM.render(
   </AppStateProvider>,
   document.getElementById('root')
 );
+
+const rootApp = document.getElementById('root');
+
+if (rootApp) {
+  ReactModal.setAppElement(rootApp);
+}
 
 reportWebVitals();
