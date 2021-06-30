06e3bbd312b78d6ec9ce72b4dfbdc9eaa4c50091
dawidrydzewski11elk <dawidrydzewski11elk@gmail.com>
2021-06-30 11:17:40 +0200

    Card test Styled

---
+++ src/components/Cards/CardOptions/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CardTestContainer = styled.div`
+  margin: auto;
+  text-align: center;
+`;
+
+export const OptionContainer = styled.div`
+  margin: 15px;
+  text-align: center;
+`;
